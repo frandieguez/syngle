da53cefcd5325a65253d9654fd75a658e25bf40b
Anton Meixome <meixome@certima.net>
2012-10-15 22:34:40 +0200

    Tradución ao galego

--- prototype/entry-editing.php
+++ prototype/entry-editing.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Syngel - Web application for creating Hunspell Synonym thesaurus</title>
+    <title>Syngle - Aplicativo Web application para a creación de thesaurus de sinónimos para Hunspell</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Web application for creating Hunspell Synonym thesaurus">
     <meta name="author" content="Fran Diéguez">
@@ -15,10 +15,10 @@
     <div class="navbar navbar-fixed-top navbar-inverse">
        <div class="navbar-inner">
          <div class="container">
-           <a class="brand" href="#">Syngel</a>
+           <a class="brand" href="#">Syngle</a>
            <div class="nav-collapse" id="main-menu">
             <ul class="nav" id="main-menu-left">
-                <li><a href="#">Synonym</a></li>
+                <li><a href="#">Sinónimo</a></li>
             </ul>
         </div>
          </div>
@@ -29,15 +29,15 @@
         <header class="jumbotron subhead" id="overview">
             <div class="row">
                 <div class="">
-                  <h1 class="span6">Entry</h1>
+                  <h1 class="span6">Entrada</h1>
                   <div class="pull-right right">
                     <div class="btn-toolbar">
                         <div class="btn-group">
                             <a href="#" class="btn" id="show-help"><i class="icon-info-sign"></i></a>
                         </div>
                         <div class="btn-group">
-                            <a href="#" class="btn" rel="tooltip" data-original-title="Go to the previous word"><i class="icon-chevron-left"></i></a>
-                            <a href="#" class="btn" rel="tooltip" data-original-title="Go to the next word"><i class="icon-chevron-right"></i></a>
+                            <a href="#" class="btn" rel="tooltip" data-original-title="Ir á palabra anterior"><i class="icon-chevron-left"></i></a>
+                            <a href="#" class="btn" rel="tooltip" data-original-title="Ir á palabra seguinte"><i class="icon-chevron-right"></i></a>
                         </div>
                     </div>
                   </div>
@@ -53,21 +53,21 @@
 
       <div class="row-fluid">
         <div class="span6 receiver">
-            <h4>Synonyms</h4>
+            <h4>Sinónimas</h4>
             <div class="synonyms clearfix">
                 <ul>
                     <li></li>
                 </ul>
             </div>
 
-            <h4>Antonyms</h4>
+            <h4>Antónimas</h4>
             <div class="antonyms clearfix">
                 <ul>
                     <li></li>
                 </ul>
             </div>
 
-            <h4>Related</h4>
+            <h4>Relacionadas</h4>
             <div class="related clearfix">
                 <ul>
                     <li></li>
@@ -76,17 +76,17 @@
         </div>
         <div class="span6" id="word-provider">
             <ul class="nav nav-tabs" id="myTab">
-                <li class="active"><a href="#server" data-toggle="tab">Server</a></li>
-                <li><a href="#suggestions" data-toggle="tab">Suggestions</a></li>
+                <li class="active"><a href="#server" data-toggle="tab">Servidor</a></li>
+                <li><a href="#suggestions" data-toggle="tab">Suxestións</a></li>
             </ul>
 
             <div class="tab-content">
                 <div class="tab-pane" id="server">
-                    Loading...
+                    Cargando...
                 </div>
                 <div class="tab-pane" id="suggestions">
                     <!-- MySQL levenshtein distance: http://www.jisaacks.com/find-similar-products-in-mysql-using-levenshtein-distance -->
-                    Loading...
+                    Cargando...
                 </div>
             </div>
 
@@ -96,13 +96,13 @@
     </div>
     <footer class="container">
         <div class="pull-left">
-            <strong>Syngel - &copy; 2012</strong> <br>
-            A product by <a href="http://www.frandieguez.com">Fran Diéguez</a>
+            <strong>Syngle - &copy; 2012</strong> <br>
+            Un produto de <a href="http://www.frandieguez.com">Fran Diéguez</a>
         </div>
         <div class="pull-right right">
             <ul>
-                <li><a href="#">Help</a></li>
-                <li><a href="#">Terms of service</a></li>
+                <li><a href="#">Axuda</a></li>
+                <li><a href="#">Termos e condicións</a></li>
             </ul>
         </div>
     </footer>
@@ -110,8 +110,8 @@
     <script id="entry_hb" type="text/x-handlebars-template">
         <div class="input-append">
           <form action="#" id="search-form">
-            <input class="span10" id="search-word" size="16" type="text" placeholder="Search for a word…" value="{{search}}">
-            <button id="search-submit" class="btn" type="submit">Search !</button>
+            <input class="span10" id="search-word" size="16" type="text" placeholder="Buscar unha palabra…" value="{{search}}">
+            <button id="search-submit" class="btn" type="submit">Buscar !</button>
           </form>
         </div>
       <ul class="nav nav-list bs-docs-sidenav" data-cursor-selectable>
@@ -121,9 +121,9 @@
             <span class="title">{{title}}</span>
             <div class="btn-toolbar pull-right">
               <div class="btn-group">
-                <span class="btn add-to-synonym btn-mini" rel="tooltip" data-original-title="Add as Synonym">S</span>
-                <span class="btn add-to-antonym btn-mini" rel="tooltip" data-original-title="Add as Antonym">A</span>
-                <span class="btn add-to-related btn-mini" rel="tooltip" data-original-title="Add as Related">R</span>
+                <span class="btn add-to-synonym btn-mini" rel="tooltip" data-original-title="Engdir como sinónima">S</span>
+                <span class="btn add-to-antonym btn-mini" rel="tooltip" data-original-title="Engadir como antónima">A</span>
+                <span class="btn add-to-related btn-mini" rel="tooltip" data-original-title="Engadir como relacionada">R</span>
               </div>
             </div>
         </a>
@@ -132,66 +132,65 @@
       </ul>
       <ul class="pager">
         <li class="previous">
-          <a href="#" data-page="{{prev_page}}" data-search-string="{{search}}">&larr; Older</a>
+          <a href="#" data-page="{{prev_page}}" data-search-string="{{search}}">&larr; Anterior</a>
         </li>
         <li class="next">
-          <a href="#" data-page="{{next_page}}" data-search-string="{{search}}">Newer &rarr;</a>
+          <a href="#" data-page="{{next_page}}" data-search-string="{{search}}">Seguinte &rarr;</a>
         </li>
       </ul>
     </script>
 
     <div id="help">
         <div>
-            <h2>Help about this page</h2>
+            <h2>Axuda sobre esta páxina</h2>
             <div class="close">X</div>
             <div class="span4">
-                <p>At this page you can stablish relationship between words to its
-                root word.</p>
-                <h6>There are some available relationships:</h6>
+                <p>Nesta páxina pode estabelecer a relación entre as palabras e a palabra raíz.</p>
+                <h6>Tipos de relacións dispoñíbeis:</h6>
                 <ul>
-                    <li>Synonym</li>
-                    <li>Antonym</li>
-                    <li>Related: for stablish other kind of relationship</li>
+                    <li>Sinonimia</li>
+                    <li>Antonimia</li>
+                    <li>Relación: para estabelecer outra clase de relacións</li>
                 </ul>
-                <h6>Getting words</h6>
-                <p>You can get elements to relate from two sources:</p>
+                <h6>Obter as palabras</h6>
+                <p>Pode obter elementos para relacionar de dúas fontes:</p>
                 <ul>
-                    <li>Searching for a word by using the «Search» form</li>
-                    <li>Go to the «Suggestions» tab for getting machine-suggested words (based on
-                    word similarity).</li>
+                    <li>Buscando unha palabra usando o formulario de «Buscar»</li>
+                    <li>Ir á lapela «Suxestións» para obter palabras suxeridas automaticamente (conforme
+                    semellanzas na palabra).</li>
                 </ul>
 
-                <p>Click over any list item for marking it as active and use the button group
-                    for performing an action.</p>
+                <p>Premer sobre calquera elemento da lista para marcalo como activo e usar e usar o botón grupo
+                    para realizar unha acción.</p>
             </div>
             <div class="span4">
-                <h5>Keyword shortcuts</h5>
-                <p>For quicker usage of this page, you can assign words by using your keyboard.</p>
+                <h5>Atallos de teclado</h5>
+                <p>Para un uso moi rápido desta páxina, pode asignar palabras usando o seu teclado.</p>
                 <dl>
                     <dt>a :</dt>
-                    <dd>Adds the active translation to the antonyms list</dd>
+                    <dd>Engade a tradución activa á lista de antónimos</dd>
                     <dt>s :</dt>
-                    <dd>Adds the active translation to the synonyms list</dd>
+                    <dd>Engade a tradución activa á lista de sinónimos</dd>
                     <dt>r :</dt>
-                    <dd>Adds the active translation to the related list</dd>
+                    <dd>Engade a tradución activa á lista de relacionadas</dd>
 
 
                     <dt>Shift + a :</dt>
-                    <dd>Adds the active translation to the antonyms list as a new concept</dd>
+                    <dd>Engade a tradución activa á lista de antónimos como novo concepto</dd>
                     <dt>Shift + s :</dt>
-                    <dd>Adds the active translation to the synonyms list as a new concept</dd>
+                    <dd>Engade a tradución activa á lista de sinónimos como novo concepto</dd>
                     <dt>Shift + r :</dt>
-                    <dd>Adds the active translation to the related list in as a new concept</dd>
+                    <dd>Engade a tradución activa á lista de relacionadas como novo concepto</dd>
                 </dl>
             </div>
 
             <div class="span4">
-                <h6>Other shortcuts</h6>
+                <h6>Outros atallos</h6>
                 <dl>
                     <dt>h :</dt>
-                    <dd>Shows this help dialog</dd>
+                    <dd>Amosa este diálogo de axuda</dd>
                     <dt>/ :</dt>
-                    <dd>Focus search input</dd>
+                    <dd>Destaca a entrada para a busca</dd>
                 </dl>
             </div>
         </div>
